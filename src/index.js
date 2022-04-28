575cfa27aca9d5fca40e5c0c60b208e4b93c3771
Jenish Patel <84110159+JRPATEL47@users.noreply.github.com>
2022-04-28 01:18:07 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-/***********************************************************************************
- * WEB422 – Assignment 3
- * I declare that this assignment is my own work in accordance with Seneca Academic Policy.
- * No part of this assignment has been copied manually or electronically from any other source
- * (including web sites) or distributed to other students.
- *
- * Name: ____Jenish Rajnikant Patel_____ Student ID: __131733206____ Date: ___11-02-2022___
- *
- ********************************************************************************/
+/*
+ * Vercel Link: https://restaurant-react-api-4zcrrjaru-jrpatel47.vercel.app/Restaurants
+*/
 
 import React from "react";
 import ReactDOM from "react-dom";
@@ -25,7 +19,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
